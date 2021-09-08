c081d0798d7957a1f1a885d61ff1f8eee54a5926
Sasky Mali <59846495+ucheothniel@users.noreply.github.com>
2021-09-08 00:25:40 -0400

    Add files via upload

--- Project 1/script.js
+++ Project 1/script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('Email');
 const password = document.getElementById('Password');
-const password2 = document.getElementById('password2');
+const password2 = document.getElementById('Password2');
 
 // Show input error message ( best practice, write the functions above the event listeners)
 function showError(input, message) {
@@ -19,10 +19,31 @@ function showSuccess(input) {
 }
 
 //check email is valid (code gotten from stack overflow)
-function isValidEmail(email) {
+/*
+function checkEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+*/
+
+// another way to validate email
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }       
+}
+
+//check passwordss match
+function checkPasswordsMatch(input1, input2) {
+    if(input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }
+
+}
+
 
 // get field name function
 function getFieldName(input) {
@@ -30,7 +51,19 @@ function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1); // select the firstletter of the input id convert to upper case, the concatinate with the rest of the input id by clicing from the second character with the index 1 
 }
 
+// check input length
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} needs to be at least ${min} characters`);
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} needs to be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
 
+}
+
+// check required fields
 function checkRequired(inputArr) {
     inputArr.forEach(function(input) {
        if(input.value.trim() === '') {
@@ -49,6 +82,10 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     checkRequired([username,email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
 });
 
 
